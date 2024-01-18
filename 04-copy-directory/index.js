d3f3686d6fdeea7ec89470be9859b154e3668e8d
stf0x9 <stf0x9@gmail.com>
2024-01-18 14:44:52 -0800

    fix: bug when removed file is still in files-copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -4,28 +4,48 @@ const path = require('path');
 const filesCopyPath = path.join(__dirname, 'files-copy');
 const filesPath = path.join(__dirname, 'files');
 
-fs.mkdir(filesCopyPath, { recursive: true }, (mkdirErr) => {
-  if (mkdirErr) {
-    console.log(mkdirErr);
+fs.access(filesCopyPath, (noAccess) => {
+  if (noAccess) {
+    addCopy();
     return;
+  } else {
+    fs.rm(filesCopyPath, {recursive: true}, (rmErr) => {
+      if (rmErr) {
+        console.error(rmErr);
+        return;
+      }
+      addCopy();
+      return;
+    })
   }
-  fs.readdir(filesPath, (readdirErr, files) => {
-    if (readdirErr) {
-      console.log(readdirErr);
+})
+
+function addCopy() {
+  fs.mkdir(filesCopyPath, { recursive: true }, (mkdirErr) => {
+    if (mkdirErr) {
+      console.error(mkdirErr);
       return;
     }
+    fs.readdir(filesPath, (readdirErr, files) => {
+      if (readdirErr) {
+        console.error(readdirErr);
+        return;
+      }
 
-    files.forEach((file) => {
-      const filePath = path.join(filesPath, file);
-      const dest = path.join(filesCopyPath, file);
+      files.forEach((file) => {
+        const filePath = path.join(filesPath, file);
+        const dest = path.join(filesCopyPath, file);
 
-      fs.copyFile(filePath, dest, (copyFileErr) => {
-        if (copyFileErr) {
-          console.log(copyFileErr);
-          return;
-        }
+        fs.copyFile(filePath, dest, (copyFileErr) => {
+          if (copyFileErr) {
+            console.error(copyFileErr);
+            return;
+          }
+        });
       });
+      console.log('Operation successful');
     });
-    console.log('Operation successful');
   });
-});
+}
+
+
